ced10e94ecbd75a4ce126e563b12d51a2be74013
Lokesh Arya <Lokesh Arya@ARYA963>
2022-03-29 10:18:30 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VisionLibrary.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
